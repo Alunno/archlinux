78c16e6282e9c9e9a5c9bdcb98aa9acc22f2bf5b
Edison Silva Filho <edisonsilvaf@gmail.com>
2024-08-20 22:16:30 -0300

    Delete conky/my-weather.sh
    q

--- conky/my-weather.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-url="api.openweathermap.org/data/2.5/weather?$(grep lat ~/.conky/variables.lst)&$(grep lon ~/.conky/variables.lst)&mode=xml&$(grep units ~/.conky/variables.lst)&$(grep lang ~/.conky/variables.lst)&$(grep appid ~/.conky/variables.lst)" 
-curl ${url} -s -o ~/.cache/weather.xml
